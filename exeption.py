a55f948b4915ac5f74a91527139c1e58416f8d1a
CristianGC006 <cogolloguzmanesteban@gmail.com>
2026-04-30 22:29:53 -0500

    Corregir la clase CustomExeptions: ajustar el constructor y eliminar la clase vacía exeptions.

--- exeption.py
+++ exeption.py
@@ -1,4 +1,11 @@
 #exepciones
+from datetime import datetime
+class CustomExeptions(Exception):
+    def __int__(self, mensaje):
+        self.mensaje=mensaje
+        self.date=datetime.now().strftime("%Y-%m-%d %H:%M")
+        super().__init__(self.mensaje)
 
-class exeptions():
+    
+    
     pass
